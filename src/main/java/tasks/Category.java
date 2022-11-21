e6cfd2b333b18606ef08477ac5a92223b30f3d27
gracekim15237 <gracekim15237@gmail.com>
2022-11-21 13:54:11 -0500

    Category and Date classes

---
+++ src/main/java/tasks/Category.java
@@ -0,0 +1,29 @@
+package tasks;
+import gateway.DBConnection;
+import javax.swing.*;
+import java.awt.*;
+import java.sql.*;
+import java.util.ArrayList;
+
+public class Category {
+
+    private final int id;
+    private static int number = 0;
+    private final String title;
+    private Task tasks[];
+    private final Date date;
+
+    public Category(String title, Date date)
+    {
+        this.title = title;
+        this.date = date;
+        this.id = number;
+        number += 1;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+    public Date getDate() { return date; }
+    public int getId() { return id; }
+}
