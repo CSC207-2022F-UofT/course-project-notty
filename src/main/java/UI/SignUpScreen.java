0cacf94c726ab1318a41a34ab9d5c4c7fa493c24
dhanyajagannathan <dhanya.jagannathan@mail.utoronto.ca>
2022-11-30 21:52:54 -0500

    fixed up the ui for login/create account functionality (changed the layouts)

--- src/main/java/UI/SignUpScreen.java
+++ src/main/java/UI/SignUpScreen.java
@@ -15,13 +15,15 @@ public class SignUpScreen extends JFrame{
     JButton SignUpButton;
     JPasswordField Password;
 
+    JButton GoBackToWelcome;
+
 
     public SignUpScreen() {
         super("Sign Up");
 
 
 
-        setLayout(new BorderLayout());
+        setLayout(null);
         setSize(600, 500);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
@@ -31,19 +33,24 @@ public class SignUpScreen extends JFrame{
         add(username1);
 
         username = new JTextField();
-        username.setBounds(200, 27, 193, 28);
+        username.setBounds(200, 27, 210, 40);
         add(username);
 
         password1 = new JLabel("Password");
-        password1.setBounds(200, 55, 70, 20);
+        password1.setBounds(200, 71, 70, 20);
         add(password1);
 
         Password = new JPasswordField();
-        Password.setBounds(200, 75, 193, 28);
+        Password.setBounds(200, 90, 210, 40);
         add(Password);
 
         SignUpButton = new JButton("Sign Up");
-        add(SignUpButton,  BorderLayout.CENTER);
+        SignUpButton.setBounds(197, 150, 200, 60);
+        add(SignUpButton);
+
+        GoBackToWelcome = new JButton("Back to Welcome");
+        GoBackToWelcome.setBounds(197, 220, 200, 60);
+        add(GoBackToWelcome);
 
         SignUpButton.addActionListener(new ActionListener(){
 
@@ -54,7 +61,7 @@ public class SignUpScreen extends JFrame{
                 boolean result = createAccountController.create();
 
                 if (result)
-                    JOptionPane.showMessageDialog(null, "Sign Up Successful");
+                    JOptionPane.showMessageDialog(null, "Sign Up Successful, please login to access your Notty");
                 else
                     JOptionPane.showMessageDialog(null, "Username taken, please try again");
 
@@ -63,6 +70,21 @@ public class SignUpScreen extends JFrame{
 
 
         });
+
+        GoBackToWelcome.addActionListener(new ActionListener(){
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                WelcomeScreen frame = new WelcomeScreen();
+                frame.setVisible(true);
+
+
+            }
+
+        });
+
+
 
 
 
