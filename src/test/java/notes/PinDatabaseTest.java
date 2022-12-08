959d7b0d48166b8361f0b61f52c2e474db200f54
Chloe Calica <chloe.calica@mail.utoronto.ca>
2022-12-08 15:17:42 -0500

    Updated PinDatabaseTest

--- src/test/java/notes/PinDatabaseTest.java
+++ src/test/java/notes/PinDatabaseTest.java
@@ -1,5 +1,6 @@
 package notes;
 
+import UI.LogInScreen;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
@@ -9,48 +10,57 @@ import gateway.NoteDataAccess;
 public class PinDatabaseTest {
     // tests pinUnpin method from the NoteDataAccess Class
     // also tests that a newly created note is not pinned
-    private final DBConnection database = new DBConnection();
     private final NoteDataAccess noteDataAccess = new NoteDataAccess();
 
-//    @Test
-//    public void noteUnpinnedAtCreation(){
-//        // Tests that when a note class is made and added in the database, it is not pinned.
-//        Note note = new Note("note1", "note1desc");
-//        noteDataAccess.insert(note);
-//        ArrayList<Note> notes = noteDataAccess.getAll();
-//        Note noteFromDatabase = notes.get(0);
-//
-//        Assertions.assertFalse(noteFromDatabase.isPinned());
-//        noteDataAccess.delete(noteFromDatabase.getTitle());
-//        notes.remove(0);
-//   }
+    @Test
+    public void noteUnpinnedAtCreation(){
+        // Tests that when a note class is made and added in the database, it is not pinned.
+        new DBConnection();
+        LogInScreen.usernameLogged = "chloe";
 
-//    @Test
-//    public void notePinnedInDatabase() {
-//        // Tests that when a note is pinned, the database reflects this change
-//        Note note = new Note("note2", "note2desc");
-//        noteDataAccess.insert(note);
-//        ArrayList<Note> notes = noteDataAccess.getAll();
-//        Note noteFromDatabase = notes.get(0);
-//        noteDataAccess.pinUnpin(noteFromDatabase.getTitle(), true);
-//
-//        Assertions.assertTrue(noteFromDatabase.isPinned());
-//        noteDataAccess.delete(noteFromDatabase.getTitle());
-//        notes.remove(0);
-//    }
+        Note note = new Note("note1", "note1desc");
+        noteDataAccess.insert(note);
+        ArrayList<Note> notes = noteDataAccess.getAll();
+        Note noteFromDatabase = notes.get(notes.size()-1);
 
-//    @Test
-//    public void noteUnpinnedInDatabase() {
-//        // Tests that when a note is unpinned, the database reflects this change
-//        Note note = new Note("note3", "note3desc");
-//        noteDataAccess.insert(note);
-//        ArrayList<Note> notes = noteDataAccess.getAll();
-//        Note noteFromDatabase = notes.get(0);
-//        noteDataAccess.pinUnpin(noteFromDatabase.getTitle(), true);
-//        noteDataAccess.pinUnpin(noteFromDatabase.getTitle(), false);
-//
-//        Assertions.assertFalse(noteFromDatabase.isPinned());
-//        noteDataAccess.delete(noteFromDatabase.getTitle());
-//        notes.remove(0);
-//    }
+        Assertions.assertFalse(noteFromDatabase.isPinned());
+        noteDataAccess.delete(noteFromDatabase.getTitle());
+   }
+
+    @Test
+    public void notePinnedInDatabase() {
+        // Tests that when a note is pinned, the database reflects this change
+        new DBConnection();
+        LogInScreen.usernameLogged = "chloe";
+
+        Note note = new Note("note2", "note2desc");
+        noteDataAccess.insert(note);
+        ArrayList<Note> notes = noteDataAccess.getAll();
+        Note noteFromDatabase = notes.get(notes.size()-1);
+        noteDataAccess.pinUnpin(noteFromDatabase.getTitle(), true);
+        notes = noteDataAccess.getAll();
+        noteFromDatabase = notes.get(notes.size()-1);
+
+        Assertions.assertTrue(noteFromDatabase.isPinned());
+        noteDataAccess.delete(noteFromDatabase.getTitle());
+    }
+
+    @Test
+    public void noteUnpinnedInDatabase() {
+        // Tests that when a note is unpinned, the database reflects this change
+        new DBConnection();
+        LogInScreen.usernameLogged = "chloe";
+
+        Note note = new Note("note3", "note3desc");
+        noteDataAccess.insert(note);
+        ArrayList<Note> notes = noteDataAccess.getAll();
+        Note noteFromDatabase = notes.get(notes.size()-1);
+        noteDataAccess.pinUnpin(noteFromDatabase.getTitle(), true);
+        noteDataAccess.pinUnpin(noteFromDatabase.getTitle(), false);
+        notes = noteDataAccess.getAll();
+        noteFromDatabase = notes.get(notes.size()-1);
+
+        Assertions.assertFalse(noteFromDatabase.isPinned());
+        noteDataAccess.delete(noteFromDatabase.getTitle());
+    }
 }
